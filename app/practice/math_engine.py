068b33213d9a41fbdffaaa61786c6390dbc28069
RishiBazz <budhaditya.rishi.dasgupta@gmail.com>
2026-03-06 12:39:51 +0100

    Update math_engine.py

--- app/practice/math_engine.py
+++ app/practice/math_engine.py
@@ -1,1 +1,42 @@
+from app.database import get_connection
+import random
 
+
+def get_math_question(user_id):
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id,
+               question_text,
+               option_a,
+               option_b,
+               option_c,
+               option_d,
+               correct_option,
+               topic,
+               difficulty
+        FROM math_question_bank
+        WHERE is_active = TRUE
+        ORDER BY RANDOM()
+        LIMIT 1
+    """)
+
+    row = cur.fetchone()
+
+    if not row:
+        return {"error": "No question found"}
+
+    return {
+        "question_id": row[0],
+        "question": row[1],
+        "options": {
+            "A": row[2],
+            "B": row[3],
+            "C": row[4],
+            "D": row[5]
+        },
+        "topic": row[7],
+        "difficulty": row[8]
+    }
