db9a770e941aecc5d86414c756449d151efb32e0
RishiBazz <budhaditya.rishi.dasgupta@gmail.com>
2026-03-06 12:20:46 +0100

    Update db.py

--- app/Migration/db.py
+++ app/Migration/db.py
@@ -1,1 +1,13 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+
+def get_conn():
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise RuntimeError("DATABASE_URL is not set")
+
+    conn = psycopg2.connect(database_url, cursor_factory=RealDictCursor)
+    conn.autocommit = False
+    return conn
